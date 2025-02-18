03381e033a313f775da2f458699633265d27a0a0
istiaquechowdhury <istiaquechowdhury430@gmail.com>
2025-02-18 21:48:06 +0600

    added google facebook login Task

---
+++ FBLogin/FbLogin/FbLogin/Controllers/NonSecuredController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FbLogin.Controllers
+{
+    public class NonSecuredController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
